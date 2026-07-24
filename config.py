d96e1a01d70d31d5105c2933ab9d390c4e2e3196
Shardendu Prakash Gupta <ramgupta639225@gmail.com>
2026-07-24 15:33:45 +0530

    Fix mail configuration

--- config.py
+++ config.py
@@ -9,7 +9,14 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT"))
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
+
+
+
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    WTF_CSRF_ENABLED = False
+
+    WTF_CSRF_ENABLED = os.getenv("WTF_CSRF_ENABLED", "False").lower() == "true"
+    
